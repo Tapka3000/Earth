c7c6c1ed2287d4bed1bca511d02fdef54c19cf5d
Tapka3000 <circle.table@yandex.ru>
2023-10-27 17:50:23 +0300

    first commit

---
+++ src/main/java/org/example/Earth.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Earth {
+    public static void main(String[] args) {
+        float ageEarth = 13000000000f;
+        int ageHuman = 1000;
+        float Difference = (float) (ageEarth / ageHuman);
+        System.out.println("В " + Difference + " раз, вселенная старше людей");
+
+
+    }
+}
