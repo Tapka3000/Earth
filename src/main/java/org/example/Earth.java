dbab63d182567ef98aeb95f77bb7fc82a1ac6883
Tapka3000 <circle.table@yandex.ru>
2023-11-02 11:03:22 +0300

    Commit

--- src/main/java/org/example/Earth.java
+++ src/main/java/org/example/Earth.java
@@ -4,9 +4,10 @@ public class Earth {
     public static void main(String[] args) {
         double ageEarth = 13000000000d;
         int ageHuman = 1000;
-        double Difference = ageEarth / ageHuman;
+        double difference = ageEarth / ageHuman;
+
         System.out.println("Возраст вселенной " + ageEarth + " лет");
         System.out.println("Возраст человека, как вида " + ageEarth + " лет");
-        System.out.println("В " + Difference + " раз,вселенная старше людей");
+        System.out.println("В " + difference + " раз,вселенная старше людей");
     }
 }
